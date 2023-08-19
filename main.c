dad1e1bb790e6f17f1e7c3c0d11a772e70a7e6c2
petrostrak <pit.trak@gmail.com>
2023-08-19 22:26:36 +0300

    Add Frame struct

--- main.c
+++ main.c
@@ -7,7 +7,14 @@
 
 #define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
 
-uint32_t global_frames[4080] = {0};
+typedef struct
+{
+    float left;
+    float right;
+} Frame;
+
+
+Frame global_frames[4080] = {0};
 size_t global_frames_count = 0;
 
 void callback(void *bufferData, unsigned int frames)
@@ -15,15 +22,15 @@ void callback(void *bufferData, unsigned int frames)
     size_t capacity = ARRAY_LEN(global_frames);
     if (frames <= capacity - global_frames_count)
     {
-        memcpy(global_frames + global_frames_count, bufferData, sizeof(uint32_t)*frames);
+        memcpy(global_frames + global_frames_count, bufferData, sizeof(Frame)*frames);
         global_frames_count += frames;
     } else if (frames <= capacity)
     {
-        memmove(global_frames, global_frames + frames, sizeof(uint32_t)*(capacity-frames));
-        memcpy(global_frames + (capacity - frames), bufferData, sizeof(uint32_t)*frames);
+        memmove(global_frames, global_frames + frames, sizeof(Frame)*(capacity-frames));
+        memcpy(global_frames + (capacity - frames), bufferData, sizeof(Frame)*frames);
     } else
     {
-        memcpy(global_frames, bufferData, sizeof(uint32_t)*capacity);
+        memcpy(global_frames, bufferData, sizeof(Frame)*capacity);
         global_frames_count = capacity;
     }
 }
@@ -36,11 +43,13 @@ int main(void)
     InitAudioDevice();
 
     Music music = LoadMusicStream("supersonic.ogg");
+    SetMusicVolume(music, 0.2);
     PlayMusicStream(music);
 
     AttachAudioStreamProcessor(music.stream, callback);
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose()) 
+    {
         UpdateMusicStream(music);
 
         if (IsKeyPressed(KEY_SPACE))
@@ -48,7 +57,8 @@ int main(void)
             if (IsMusicStreamPlaying(music))
             {
                 PauseMusicStream(music);
-            } else {
+            } else 
+            {
                 ResumeMusicStream(music);
             }
             
@@ -63,15 +73,12 @@ int main(void)
         float cell_width = (float)w/global_frames_count;
         for (size_t i = 0; i < global_frames_count; i++)
         {
-            int16_t sample = *(int16_t*)&global_frames[i];
-            
-            if (sample > 0)
+            float t = global_frames[i].left;
+            if (t > 0)
             {
-                float  t = (float)sample/INT16_MAX;
                 DrawRectangle(i*cell_width, h/2 - h/2*t, 1, h/2*t, RED);
             } else
             {
-                float  t = (float)sample/INT16_MIN;
                 DrawRectangle(i*cell_width, h/2, 1, h/2*t, RED);
             }
         }

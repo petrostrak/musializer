558d8a47efc6263baaa72be6ac5fa95595fc11bc
petrostrak <pit.trak@gmail.com>
2023-08-20 11:16:11 +0300

    implement fft and dft

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <complex.h>
+#include <assert.h>
 
 #include <raylib.h>
 
@@ -32,6 +34,43 @@ void callback(void *bufferData, unsigned int frames)
     {
         memcpy(global_frames, bufferData, sizeof(Frame)*capacity);
         global_frames_count = capacity;
+    }
+}
+
+void dft(float in[], float complex out[], size_t n)
+{
+    for (size_t f = 0; f < n; f++)
+    {
+        out[f] = 0;
+        for (size_t i = 0; i < n; i++)
+        {
+            float t = (float)i/n;
+            out[f] += in[i]*cexp(2*I*PI*f*t);
+        }
+        
+    }
+    
+}
+
+void fft(float in[], size_t stride, float complex out[], size_t n)
+{
+    assert(n > 0); 
+    if (n == 1) 
+    {
+        out[0] = in[0];
+        return;
+    }
+    
+    fft(in, stride*2, out, n/2);
+    fft(in + stride, stride*2, out + n/2, n/2);
+
+    for (size_t k = 0; k < n/2; k++)
+    {
+        float t = (float)k/n;
+        float complex v = cexp(-2*I*PI*t)*out[k+n/2];
+        float complex e = out[k];
+        out[k] = e + v;
+        out[k+n/2] = e - v;
     }
 }
 
